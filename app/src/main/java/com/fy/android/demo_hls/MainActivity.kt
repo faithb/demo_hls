b522693b90c86397a08d29e9c9f0247a79a987e3
Hung Tran V <hung.tran@MBP0005.local>
2022-09-16 12:46:19 +0700

    first commit

---
+++ app/src/main/java/com/fy/android/demo_hls/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.fy.android.demo_hls
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fy.android.demo_hls.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var exoPlayer: ExoPlayer? = null
+    private var playbackPosition = 0L
+    private var playWhenReady = true
+
+    companion object {
+        const val URL =
+            "https://cdn.bitmovin.com/content/assets/art-of-motion-dash-hls-progressive/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        preparePlayer()
+    }
+
+    private fun preparePlayer() {
+        exoPlayer = ExoPlayer.Builder(this).build()
+        exoPlayer?.playWhenReady = true
+        binding.videoView.player = exoPlayer
+        val defaultHttpDataSourceFactory = DefaultHttpDataSource.Factory()
+        val mediaItem =
+            MediaItem.fromUri(URL)
+        val mediaSource =
+            HlsMediaSource.Factory(defaultHttpDataSourceFactory).createMediaSource(mediaItem)
+        exoPlayer?.setMediaSource(mediaSource)
+        exoPlayer?.seekTo(playbackPosition)
+        exoPlayer?.playWhenReady = playWhenReady
+        exoPlayer?.prepare()
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.let { player ->
+            playbackPosition = player.currentPosition
+            playWhenReady = player.playWhenReady
+            player.release()
+            exoPlayer = null
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+}
